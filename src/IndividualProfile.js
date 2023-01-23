6011a163b8873db02babb02da569107d0ccc7cf8
manasaling <lgmanasa@gmail.com>
2023-01-22 20:52:28 -0800

    fixed text size + icon issue yay

--- src/IndividualProfile.js
+++ src/IndividualProfile.js
@@ -51,16 +51,16 @@ const IndividualProfile = () => {
 
                             <p className="w3-large w3-text-theme"><b><i className="fa fa-globe fa-fw w3-margin-right icon-blue"></i>Interests</b></p>
                             <p>Healthcare</p>
-                            <div className="w3-light-grey w3-round-xlarge">
-                            <div className="w3-round-xlarge connect-purple" ></div>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                            <div className="w3-container w3-center w3-round-xlarge connect-purple">80%</div>
                             </div>
                             <p>Education</p>
-                            <div className="w3-light-grey w3-round-xlarge">
-                            <div className="w3-round-xlarge connect-purple" ></div>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                            <div className="w3-container w3-center w3-round-xlarge connect-purple">50%</div>
                             </div>
                             <p>Justice</p>
-                            <div className="w3-light-grey w3-round-xlarge">
-                            <div className="w3-round-xlarge connect-purple"></div>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                            <div className="w3-container w3-center w3-round-xlarge connect-purple">40%</div>
                             </div>
                         </div>
                         </div>
@@ -71,19 +71,19 @@ const IndividualProfile = () => {
                         <div className="w3-container w3-card w3-white w3-margin-bottom">
                         <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge icon-purple"></i>Experience</h2>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Front End Developer / w3schools.com</b></h5>
+                            <h3 className="w3-opacity w3-text-black"><b>Front End Developer / w3schools.com</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>Jan 2015 - Current</h6>
                             <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
 
                         </div>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Web Developer / something.com</b></h5>
+                            <h3 className="w3-opacity w3-text-black" ><b>Web Developer / something.com</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>Mar 2012 - Dec 2014</h6>
                             <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
 
                         </div>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
+                            <h3 className="w3-opacity w3-text-black"><b>Graphic Designer / designsomething.com</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>Jun 2010 - Mar 2012</h6>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
                         </div>
@@ -92,19 +92,19 @@ const IndividualProfile = () => {
                         <div className="w3-container w3-card w3-white">
                         <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-certificate fa-fw w3-margin-right w3-xxlarge icon-purple"></i>Education</h2>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>W3Schools.com</b></h5>
+                            <h3 className="w3-opacity w3-text-black"><b>W3Schools.com</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>Forever</h6>
                             <p>Web Development! All I need to know in one place</p>
 
                         </div>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>London Business School</b></h5>
+                            <h3 className="w3-opacity w3-text-black"><b>London Business School</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>2013 - 2015</h6>
                             <p>Master Degree</p>
 
                         </div>
                         <div className="w3-container">
-                            <h5 className="w3-opacity"><b>School of Coding</b></h5>
+                            <h3 className="w3-opacity w3-text-black"><b>School of Coding</b></h3>
                             <h6 className="w3-text-black"><i className="fa fa-calendar fa-fw w3-margin-right icon-blue"></i>2010 - 2013</h6>
                             <p>Bachelor Degree</p>
                         </div>
