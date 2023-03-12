a65b495608f4d33054bce2699616707757661e06
pranatidani-uw <88809465+pranatidani-uw@users.noreply.github.com>
2023-03-11 21:38:52 -0800

    added in some code for filtering backend

--- src/ProfileList.js
+++ src/ProfileList.js
@@ -77,6 +77,59 @@ const ProfileList = ({location}) => {
 
   //console.log(all_user);
 
+
+  /*
+      do we want the checkbox code to be in a seperate file that renders the whole profiles list page again if needed?
+  */
+
+
+  // testing some things out for tomorro
+
+  /*
+    some code for checklists:
+
+    import React, { useState } from 'react';
+
+    function Checklist() {
+      const [selectedValues, setSelectedValues] = useState([]);
+
+      const handleCheckboxChange = (event) => {
+        const value = event.target.value;
+        const isChecked = event.target.checked;
+
+        if (isChecked) {
+          setSelectedValues([...selectedValues, value]); // add the selected value to the state
+        } else {
+          setSelectedValues(selectedValues.filter((v) => v !== value)); // remove the unselected value from the state
+        }
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(selectedValues); // output the selected values to the console
+        // TODO: send the selected values to the server
+      };
+
+      return (
+        <form onSubmit={handleSubmit}>
+          <input type="checkbox" value="option1" onChange={handleCheckboxChange} />
+          <label>Option 1</label>
+
+          <input type="checkbox" value="option2" onChange={handleCheckboxChange} />
+          <label>Option 2</label>
+
+          <input type="checkbox" value="option3" onChange={handleCheckboxChange} />
+          <label>Option 3</label>
+
+          <button type="submit">Submit</button>
+        </form>
+      );
+    }
+
+
+  */
+
+
   return (
     <div>
       <Layout></Layout>
@@ -97,7 +150,17 @@ const ProfileList = ({location}) => {
         <profile>
           {[...all_user.entries()].map(([userId, userData]) => {
               //console.log(userData);
-              return <ProfileCard myObject={userData}> </ProfileCard>
+
+              // i think we can somehow get a list of attributes for each type that we want, have them compared with what we have here without casing
+              // and a check if that 'list' is empty
+
+              // that table that comes from the checklist of the filters needs to be stored as a lower case only array
+
+              // list names for now: skillsConstraints, interestsConstraints
+              if ((skillsConstraints.length == 0 || skillsConstraints.some(value => (userData.skills).includes(value.toLowerCase()))) || (interestsConstraints.length == 0 || interestsConstraints.some(value => (userData.interests).includes(value.toLowerCase())))) {
+                // this checks to see if some values of one array and in the other array, meaning we would want to include it
+                return <ProfileCard myObject={userData}> </ProfileCard>
+              }
           })}
         </profile>
       
