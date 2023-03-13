3b6bb460db3b40a15a38f7b05b0c697bb1c8fbd2
pranatidani-uw <88809465+pranatidani-uw@users.noreply.github.com>
2023-03-12 19:23:12 -0700

    done

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,43 +1,87 @@
 import React from 'react';
+import ProfileList from './ProfileList';
 import './SideBar.css';
 
 const SideBar = () => {
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const element = document.querySelector('#element-id');
-        element.addEventListener('click', function() {
-          // code to run when the element is clicked
-          const nameFilterInput = document.getElementById('nameFilterInput');
-          const majorFilterInput = document.getElementById('majorFilterInput');
-          const skillsFilterInput = document.getElementById('skillsFilterInput');
-          const interestsFilterInput = document.getElementById('interestsFilterInput');
-          const filteredItemsDiv = document.getElementById('filteredItems');
+
+    const skillsArray = [];
+    
+
+    const addFilterName = (event) => {
+        if (event.key == 'Enter') {
+            //const nameFilterInput = document.getElementById("nameFilterInput");
+            const skillsFilterInput = document.getElementById("skillsFilterInput");
+            const filteredItemsDiv = document.getElementById("filteredItems");
+            const skillsFilter = skillsFilterInput.value;
+            // const majorFilter = majorFilterInput.value;
+            // const skillsFilter = skillsFilterInput.value;
+            // const interestsFilter = interestsFilterInput.value;
+    
+            const filteredItem = document.createElement('div');
+            filteredItem.innerText = `${skillsFilter}`;
+            filteredItemsDiv.appendChild(filteredItem);
+
+            skillsArray.push(skillsFilter);
+    
+            // Clear the input fields after adding the filter
+            skillsFilterInput.value = '';
+            // majorFilterInput.value = '';
+            // skillsFilterInput.value = '';
+            // interestsFilterInput.value = '';
+        }
+        }
+
+        const renderPageAgain = () => {
+
+            console.log("we are in render");
+            const filterArr = [];
+            filterArr.push(skillsArray);
+
+                          
+
+            
+            return (<ProfileList myObject={filterArr}> </ProfileList>);
+
+            
+        }
+
+
+
+    // document.addEventListener('DOMContentLoaded', function() {
+
+    //       // code to run when the element is clicked
+    //       const nameFilterInput = document.getElementById('nameFilterInput');
+    //       const majorFilterInput = document.getElementById('majorFilterInput');
+    //       const skillsFilterInput = document.getElementById('skillsFilterInput');
+    //       const interestsFilterInput = document.getElementById('interestsFilterInput');
+    //       const filteredItemsDiv = document.getElementById('filteredItems');
       
-          nameFilterInput.addEventListener('keyup', addFilter);
-          majorFilterInput.addEventListener('keyup', addFilter);
-          skillsFilterInput.addEventListener('keyup', addFilter);
-          interestsFilterInput.addEventListener('keyup', addFilter);
+    //       nameFilterInput.addEventListener('keyup', addFilterName);
+    //     //   majorFilterInput.addEventListener('keyup', addFilter);
+    //     //   skillsFilterInput.addEventListener('keyup', addFilter);
+    //     //   interestsFilterInput.addEventListener('keyup', addFilter);
       
-          function addFilter(event) {
-          if (event.key == 'Enter') {
-              const nameFilter = nameFilterInput.value;
-              const majorFilter = majorFilterInput.value;
-              const skillsFilter = skillsFilterInput.value;
-              const interestsFilter = interestsFilterInput.value;
+    //       function addFilterName(event) {
+    //       if (event.key == 'Enter') {
+    //           const nameFilter = nameFilterInput.value;
+    //           const majorFilter = majorFilterInput.value;
+    //           const skillsFilter = skillsFilterInput.value;
+    //           const interestsFilter = interestsFilterInput.value;
       
-              const filteredItem = document.createElement('div');
-              filteredItem.innerText = `Name: ${nameFilter}, Major: ${majorFilter}, Skills: ${skillsFilter}, Interests: ${interestsFilter}`;
-              filteredItemsDiv.appendChild(filteredItem);
+    //           const filteredItem = document.createElement('div');
+    //           filteredItem.innerText = `Name: ${nameFilter}, Major: ${majorFilter}, Skills: ${skillsFilter}, Interests: ${interestsFilter}`;
+    //           filteredItemsDiv.appendChild(filteredItem);
       
-              // Clear the input fields after adding the filter
-              nameFilterInput.value = '';
-              majorFilterInput.value = '';
-              skillsFilterInput.value = '';
-              interestsFilterInput.value = '';
-          }
-          }
-        });
-      });
+    //           // Clear the input fields after adding the filter
+    //           nameFilterInput.value = '';
+    //           majorFilterInput.value = '';
+    //           skillsFilterInput.value = '';
+    //           interestsFilterInput.value = '';
+    //       }
+    //       }
+
+    //   });
 
 
 
@@ -49,14 +93,14 @@ const SideBar = () => {
         Major 
         <input type="text" id="majorFilterInput" placeholder="Enter in Major Filters.."></input>
         Skils 
-        <input type="text" id="skillsFilterInput" placeholder="Enter in Skills Filters.."></input>
+        <input type="text" id="skillsFilterInput" placeholder="Enter in Skills Filters.." onKeyUp={addFilterName}></input>
         Interests 
         <input type="text" id="interestsFilterInput" placeholder="Enter in Interests Filters.."></input>
 
 
-        <button onClick="renderPageAgain">Enter Filters</button>
+        <button id="filterButton" onClick={renderPageAgain}>Enter Filters</button>
 
-        <div class="filteredItems">initial</div>
+        <div id="filteredItems">initial</div>
 
     </div>
 
